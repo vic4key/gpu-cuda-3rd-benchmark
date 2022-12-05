4681ca60948e54cc9b52c39195c4fecdb8cf055d
Vic P <vic4key@gmail.com>
2022-12-05 16:19:36 +0700

    Initial

--- Setup.cmd
+++ Setup.cmd
@@ -4,10 +4,6 @@ ECHO Initializing...
 ECHO.
 
 git submodule update --remote --progress --init --recursive --force
-rem git submodule update --progress --init --recursive --force
-rem git submodule update --init --recursive
-rem git submodule foreach --recursive git checkout master
-rem git submodule foreach --recursive git pull
 
 ECHO.
 ECHO Finished
